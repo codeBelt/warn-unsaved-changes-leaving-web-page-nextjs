3735727b0b7f8baf090c6256289db01c2402cbd1
robert <robertsavian@gmail.com>
2023-02-04 19:48:26 -0600

    changesOnPage

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -3,29 +3,29 @@ import { Switch } from '@headlessui/react';
 import { useLeavePageConfirmation } from '@/hooks/useLeavePageConfirmation';
 
 export default function FormRoute() {
-  const [enabled, setEnabled] = useState(false);
+  const [changesOnPage, setChangesOnPage] = useState(false);
 
-  useLeavePageConfirmation(enabled);
+  useLeavePageConfirmation(changesOnPage);
 
   return (
     <div className="py-16">
       <Switch.Group>
         <div className="flex items-center">
           <Switch.Label className="mr-4">
-            {enabled
+            {changesOnPage
               ? 'Changes on page (Try to navigate away, refresh, close, or click the back button)'
               : 'No changes on page (You can navigate freely)'}
           </Switch.Label>
           <Switch
-            checked={enabled}
-            onChange={setEnabled}
+            checked={changesOnPage}
+            onChange={setChangesOnPage}
             className={`${
-              enabled ? 'bg-blue-600' : 'bg-gray-200'
+              changesOnPage ? 'bg-blue-600' : 'bg-gray-200'
             } relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}
           >
             <span
               className={`${
-                enabled ? 'translate-x-6' : 'translate-x-1'
+                changesOnPage ? 'translate-x-6' : 'translate-x-1'
               } inline-block h-4 w-4 transform rounded-full bg-white transition-transform`}
             />
           </Switch>
