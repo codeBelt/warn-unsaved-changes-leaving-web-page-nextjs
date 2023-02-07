f36c7fbcff4cfec3becaa46a66f7664de73261e1
robert <robertsavian@gmail.com>
2023-02-07 11:34:52 -0600

    add article link

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -40,6 +40,7 @@ export const MainNav = (props) => {
           <a
             href="https://betterprogramming.pub/prevent-route-changes-and-unsaved-data-loss-in-next-js-f93622d73791"
             target={'_blank'}
+            rel="noreferrer"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
           >
             Read My Article
